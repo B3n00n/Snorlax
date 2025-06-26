df70f1ace6a2795293e8f28f46030b8e2be76c58
B3n00n <benbahar321@gmail.com>
2025-06-26 13:45:00 +0300

    removed shit

--- app/src/main/java/com/b3n00n/snorlax/MainActivity.kt
+++ app/src/main/java/com/b3n00n/snorlax/MainActivity.kt
@@ -2,23 +2,11 @@ package com.b3n00n.snorlax
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Build
-import android.os.PowerManager
-import android.provider.Settings
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val pm = getSystemService(POWER_SERVICE) as PowerManager
-            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
-                val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
-                intent.data = android.net.Uri.parse("package:$packageName")
-                startActivity(intent)
-            }
-        }
 
         val serviceIntent = Intent(this, RemoteClientService::class.java)
         startService(serviceIntent)
