bfa65d17cad9af857a7923977e5d6d09abc88ad3
Benoon <72699618+B3n00n@users.noreply.github.com>
2025-09-07 16:08:37 +0300

    cleanup

--- server/core/http_server.py
+++ server/core/http_server.py
@@ -59,7 +59,6 @@ class APKHttpServer:
     def get_local_ip(self) -> str:
         if not self._local_ip:
             try:
-                # Create a dummy socket to determine local IP
                 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 s.connect(("8.8.8.8", 80))
                 self._local_ip = s.getsockname()[0]
