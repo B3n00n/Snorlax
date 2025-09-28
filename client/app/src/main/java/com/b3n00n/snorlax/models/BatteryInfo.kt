d5e15b7ed67d720c64ed8fb810cde990771b5ce8
Benoon <72699618+B3n00n@users.noreply.github.com>
2025-09-28 17:05:57 +0300

    future support for controllers batteries streaming, FOR WHEN META START ACTUALLY DOING SHIT PROPERLY

--- client/app/src/main/java/com/b3n00n/snorlax/models/BatteryInfo.kt
+++ client/app/src/main/java/com/b3n00n/snorlax/models/BatteryInfo.kt
@@ -2,5 +2,7 @@ package com.b3n00n.snorlax.models
 
 data class BatteryInfo(
     val headsetLevel: Int,
-    val isCharging: Boolean
+    val isCharging: Boolean,
+    val leftControllerLevel: Int? = null,
+    val rightControllerLevel: Int? = null
 )
