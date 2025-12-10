b183679ae25998240b5a2a103b43472b85d4e0af
Benoon <72699618+B3n00n@users.noreply.github.com>
2025-12-10 13:57:21 +0200

    bump rev

--- client/app/src/main/java/com/b3n00n/snorlax/config/SnorlaxConfigManager.kt
+++ client/app/src/main/java/com/b3n00n/snorlax/config/SnorlaxConfigManager.kt
@@ -1,7 +1,7 @@
 package com.b3n00n.snorlax.config
 
 object SnorlaxConfigManager {
-    const val APP_VERSION = "1.1.7"
+    const val APP_VERSION = "1.2.0"
 
     const val APP_NAME = "Snorlax"
     const val DEVELOPER = "B3n00n"
